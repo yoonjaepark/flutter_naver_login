7ce0150d4aae69844efeb56bc6e5c68d29be1eb8
donald.park <rhkdro@gmail.com>
2019-11-16 02:42:30 +0900

    delete ios env

--- example/ios/Flutter/flutter_export_environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/yoonjaepark/development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/yoonjaepark/Documents/dev/flutter_naver_login/example"
-export "FLUTTER_TARGET=lib/main.dart"
-export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/yoonjaepark/development/flutter/bin/cache/artifacts/engine/ios"
